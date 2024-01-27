8a89262c060353f6525a0bfd1872beada603a9ee
William Wang <bn.william.w@gmail.com>
2024-01-27 00:35:14 -0500

    support image

--- memorySearch.py
+++ memorySearch.py
@@ -13,12 +13,15 @@ api_key = os.getenv('COHERE_API_KEY')
 co = cohere.Client(api_key)
 # Example dictionary mapping descriptions to videos
 video_dict = {
-    "Ian sleeping on the ground at a hackathon.": "Video 4 URL",
+    "Ian sleeping on the ground at a hackathon.": "Vhttps://media.discordapp.net/attachments/1161450447807193091/1200673767542771782/IMG_8059.jpg?ex=65c70995&is=65b49495&hm=42ce11fbddd04798e3a114200b9b9a9424dd8b49f7751aa95499df53eefb2e2f&=&format=webp&width=712&height=948",
     "William doing karaoke with Ian at Deltahacks": "https://cdn.discordapp.com/attachments/1189032378354651216/1195977651509264425/IMG_7435.mov?ex=65bf2e7c&is=65acb97c&hm=1530129896a85d0c0e69473fa8bc333fa51370a70b22e70a357ae9d0ceaa578a&",
     "William and Ian doing karaoke at a hackathon.": "https://cdn.discordapp.com/attachments/1176251472791478300/1200606493028909217/PXL_20240127_010105877.mp4?ex=65c6caee&is=65b455ee&hm=8e83f1ca999ed2f9af718848b9ed9680edc222a1a66ef3bf0899e69e1fbf2753&",
     "William exploring the Creepy UofT Engineering building with Ian and Lucy": "Video 6 URL",
+    "Teddy.ai saying daddy at deltahacks and making the crowd laugh": "https://cdn.discordapp.com/attachments/1161450447807193091/1200673768171901030/IMG_1642.mov?ex=65c70995&is=65b49495&hm=dc4eeab2139b33123cdfad649edee5cb3a2db18e52a51a25db8f08e22e589f92&",
+    "UoftHacks found this random party at Skule": "https://cdn.discordapp.com/attachments/1161450447807193091/1200673768633290762/IMG_8053.mov?ex=65c70995&is=65b49495&hm=50b9d58e42f21e4e33a644eb1f4a31405be4e2bcb08bc1d12e49cf8295f51afc&",
     "William going to UoftHacks, hackathon with 500+ hackers!": "https://cdn.discordapp.com/attachments/1189032378354651216/1200595671460360222/IMG_8044.mov?ex=65c6c0da&is=65b44bda&hm=0e3011747c6562b1fcc2af8a6f335b73111e5306a07ad078707906e84b367cd6&",
     "William doing karaoke at Hack the Valley": "https://cdn.discordapp.com/attachments/1176251472791478300/1200181987307176097/IMG_4867.mov?ex=65c53f94&is=65b2ca94&hm=5060449bd85e32575c6c01799a0f14d7eaec8fb73c26ec2b2eec734d8f72c746&",
+    "spicy noodle challenge at Delthacks": "https://media.discordapp.net/attachments/1189032378354651216/1195874662073241682/IMG_7429.jpg?ex=65bece91&is=65ac5991&hm=8ce7cdb1c7cb2e580abdc950836c5e44b2e54a71d5af6da8f0802537cfd5f878&=&format=webp&width=712&height=948",
     # Add more descriptions and corresponding video URLs as needed
 }
 
@@ -53,7 +56,15 @@ def main():
                 with columns[i - 1]:
                     st.write(f"{i}. Description: {description}")
                     st.write(f"Relevance: {relevance_score}")
-                    st.video(video_url)
+                    if ".mov" in video_url or ".mp4" in video_url:
+                        st.video(video_url)  # Display the video
+
+                    # Check if video_url contains ".jpg"
+                    elif ".jpg" in video_url:
+                        st.image(video_url)  # Display the image
+
+                    else:
+                        st.write("File retrieval error.")
 
 if __name__ == "__main__":
     main()
