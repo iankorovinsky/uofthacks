bbf297831968b35bdd5f34aa1ec06eeec2d6b37a
stephen <stephen@dropbase.io>
2024-01-27 01:53:12 -0500

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import React, { Suspense } from "react";
+import { AnimatePresence } from 'framer-motion'
+ 
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Create from './pages/Create'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      s
-    </div>
-  );
+    <div className="App bg-white">
+    <BrowserRouter>
+        <Suspense fallback={<div>Page Loading...</div>}>
+        <Navbar />
+        <AnimatePresence
+         mode='wait'>
+           <Routes>
+             <Route path="/" exact element={<Home />} />
+             <Route path="/create" exact element={<Create />} />
+           </Routes>
+        </AnimatePresence>
+        </Suspense>
+    </BrowserRouter>
+  </div>
+  )
 }
 
-export default App;
-
-
-
+export default App
